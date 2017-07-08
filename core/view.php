5e6425ee985391cacac56c6613c0e0423810bcd1
ProgMiner <eridan200@mail.ru>
2017-07-09 02:31:37 +0300

    Added MVC directories, fixed path in view.php

--- core/view.php
+++ core/view.php
@@ -6,7 +6,7 @@ class View{
     function generate($content_view, $template_view = $this->template_view, $data = null){
         if(is_array($data)) extract($data); // преобразуем элементы массива в переменные
         
-        include '../views/' . $template_view;
+        include 'core/views/' . $template_view;
     }
 
 }
