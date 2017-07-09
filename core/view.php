e1e7b633e21a4d30d27a9f565e7ed5f381b3c6e5
ProgMiner <eridan200@mail.ru>
2017-07-09 14:30:08 +0300

    view.php

--- core/view.php
+++ core/view.php
@@ -4,23 +4,23 @@ class View{
     public $template_view = 'main.php';
 
     /*
-	$content_file - виды отображающие контент страниц;
-	$template_file - общий для всех страниц шаблон;
-	$data - массив, содержащий элементы контента страницы. Обычно заполняется в модели.
-	*/
+        $content_file - шаблон содержимого страницы;
+        $template_file - общий шаблон страницы;
+        $data - массив, содержащий данные модели. Обычно заполняется в модели.
+    */
     
     function generate($content_view, $template_view = '', $data = null){
 
-        if(empty($template_view)) $template_view = $this->template_view;
+        if(empty($template_view)) $template_view = $this->template_view; 
         $content_view = 'core/views/' . $content_view;
 
-        if(is_array($data)) extract($data); // преобразуем элементы массива в переменные
+        if(is_array($data)) extract($data); // Преобразуем элементы массива в переменные
         
         /*
-		динамически подключаем общий шаблон (вид),
-		внутри которого будет встраиваться вид
-		для отображения контента конкретной страницы.
-		*/
+            Динамически подключаем общий шаблон (вид),
+            внутри которого будет встраиваться вид
+            для отображения контента конкретной страницы.
+        */
         include 'core/views/templates/' . $template_view;
         
     }
