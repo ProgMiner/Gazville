f03cc6d56d32e5abbbdd64f607d8d4ae1c811770
Lantos <vance8976@gmail.com>
2017-07-17 04:17:02 +0300

    Edit Login Controller

--- controllers/controller_login.php
+++ controllers/controller_login.php
@@ -1,2 +1,19 @@
 <?
+class Controller_Login extends Controller{
 
+
+    public function start(){
+
+    	if(User::isUserLoggedIn()) {
+    		Util::redirect("/");
+    	}
+
+        if(isset($_POST['login']) && isset(md5($_POST['password'])) {
+        	User::login();
+        }
+    	
+    	$this->view = new View("login.php");
+        $this->view->place();
+    }
+
+}
