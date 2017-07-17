f21b21b8ceddf4a1dff74998a229bfe26a3f40fd
Lantos <vance8976@gmail.com>
2017-07-18 02:57:02 +0300

    Add and Edit controller_admin.php

---
+++ controllers/controller_admin.php
@@ -0,0 +1,42 @@
+<?
+//Route::loadModel("Admin");
+
+class Controller_Admin extends Controller{
+
+        public function hub(){
+
+        $data = array();
+
+
+
+
+
+        $this->view = new View($data, "admin/hub.php");
+        $this->view->place();
+
+    }
+
+    public function users(){
+
+        $data = array();
+
+
+
+
+
+        $this->view = new View($data, "admin/users.php");
+        $this->view->place();
+    }
+
+    public function start(){
+
+        if(User::isUserCan(User::$permission['adminpanel'])) 
+        Util::redirect("/404", __FILE__, __LINE__); 
+
+        if(!(isset($this->argument[2]) || empty($this->argument[2]) ||
+        !method_exists($this, 'hub')) $this->argument[2] = "hub";
+        
+
+    }
+
+}
