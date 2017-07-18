44650d381906e9679fd7cddb0a5eba0d5f820c19
Lantos <vance8976@gmail.com>
2017-07-18 03:30:01 +0300

    Fix code

--- controllers/controller_admin.php
+++ controllers/controller_admin.php
@@ -33,9 +33,10 @@ class Controller_Admin extends Controller{
         if(User::isUserCan(User::$permission['adminpanel'])) 
         Util::redirect("/404", __FILE__, __LINE__); 
 
-        if(!(isset($this->argument[2]) || empty($this->argument[2]) ||
-        !method_exists($this, $this->argument[2])) $this->argument[2] = "hub";
+        if(!isset($this->argument[2]) || empty($this->argument[2]) ||
+            !method_exists($this, $this->argument[2])) $this->argument[2] = "hub";
 
+        $method = $this->argument[2];
         $this->$method();
     }
 
