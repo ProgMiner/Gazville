af7c43b4facdac52096773ae2a3f287cde235e17
Lantos <vance8976@gmail.com>
2017-07-18 03:52:15 +0300

    Edit start function

--- controllers/controller_admin.php
+++ controllers/controller_admin.php
@@ -3,7 +3,7 @@
 
 class Controller_Admin extends Controller{
 
-        public function hub(){
+    public function hub(){
 
         $data = array();
 
@@ -30,8 +30,8 @@ class Controller_Admin extends Controller{
 
     public function start(){
 
-        if(User::isUserCan(User::$permission['adminpanel'])) 
-        Util::redirect("/404", __FILE__, __LINE__); 
+        if(User::isCurrentUserCan(User::$permission['adminpanel'])) 
+            Util::error404(__FILE__, __LINE__); 
 
         if(!isset($this->argument[2]) || empty($this->argument[2]) ||
             !method_exists($this, $this->argument[2])) $this->argument[2] = "hub";
