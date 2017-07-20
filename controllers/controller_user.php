a7f316da243c7eb7e2e924f4f806f65f38903efc
Lantos <vance8976@gmail.com>
2017-07-21 01:44:45 +0300

    Add & Edit User Controller file

---
+++ controllers/controller_user.php
@@ -0,0 +1,21 @@
+<?
+class Controller_User extends Controller{
+
+    public function start(){
+
+        if(!User::isUserLoggedIn()) Util::error404();
+
+        $data = array();
+
+        //$this->view = new View($data, "user");
+        //$this->view->place();
+    }
+
+    public static function preroute($url){
+
+        if(!preg_match("/^\/(\w{,255})$/", $url, $matches)) return $url;
+        if(($id = Model_User::getIdByLogin($matches[1])) === false) return $url;
+    
+        return "/{$id}"; 
+    }
+}
